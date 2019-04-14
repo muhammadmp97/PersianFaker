41e70d1bc0ab848a4ce42e05edbb5492e0976128
WebPajooh <mohammadmp7676@gmail.com>
2019-04-14 17:03:44 +0430

    Added: Phone field

---
+++ Fields/Phone.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'Base.php';
+
+class Phone extends Base
+{
+	public static function generate(array $args=null)
+	{
+		if ($args['withCountryCode']) {
+			$phone = '+98';
+		}else{
+			$phone = '0';
+		}
+		$phone .= '992';
+		for ($i=1; $i<=7; $i++)
+			$phone .= mt_rand(0, 9);
+		return $phone;
+	}
+}
